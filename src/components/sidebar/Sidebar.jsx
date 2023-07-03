eca4eb3f2e5c33efcde6f15078fd5e1a2e2834eb
Ananth9323 <95201866+Ananth9323@users.noreply.github.com>
2023-07-04 00:51:48 +0530

    Update Sidebar.jsx

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -10,7 +10,7 @@ export default function Sidebar() {
 
     useEffect(() => {
         const getCats = async () => {
-            const res = await axios.get("/categories");
+            const res = await axios.get("https://bloggium.onrender.com/categories");
             setCats(res.data);
         };
         getCats();
